023aa2770e1d44466e8c2b211f7637690763c5f1
MacBook Pro User <macbookprouser@MacBooks-MacBook-Pro.local>
2021-03-01 16:40:19 -0500

    Disabled Push Notification and Changed Bundle Identifier

--- App.js
+++ App.js
@@ -5,26 +5,26 @@ import MainNavigation from './src/navigation/main_navigation';
 import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      Alert.alert(
-        remoteMessage.notification.title,
-        remoteMessage.notification.body,
-      );
-    });
-    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-      console.log('Message handled in the background!', remoteMessage);
-    });
-    const authStatus = messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  // useEffect(() => {
+  //   const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+  //     Alert.alert(
+  //       remoteMessage.notification.title,
+  //       remoteMessage.notification.body,
+  //     );
+  //   });
+  //   messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  //     console.log('Message handled in the background!', remoteMessage);
+  //   });
+  //   const authStatus = messaging().requestPermission();
+  //   const enabled =
+  //     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+  //     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-    if (enabled) {
-      console.log('Authorization status:', authStatus);
-    }
-    return unsubscribe;
-  }, []);
+  //   if (enabled) {
+  //     console.log('Authorization status:', authStatus);
+  //   }
+  //   return unsubscribe;
+  // }, []);
   return <MainNavigation />;
 };
 
